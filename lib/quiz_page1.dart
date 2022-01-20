72f725e28c8233a4b2ea8c3242e55bf256f92819
AmanSingh <aman.singh@grappus.com>
2022-01-20 23:55:07 +0530

    quiz app works fine with use of two lists

--- lib/quiz_page1.dart
+++ lib/quiz_page1.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
-
 class QuizApp1 extends StatefulWidget {
   const QuizApp1({Key? key}) : super(key: key);
 
@@ -11,17 +8,27 @@ class QuizApp1 extends StatefulWidget {
 }
 
 class _QuizApp1State extends State<QuizApp1> {
+  List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    "shiva is always with you",
+    "he will not always help you",
+    "you're a noob",
+    "i am pro",
+  ];
+  List<bool> answers = [true, false, false, true];
+  late int questionNumber = 0;
+   late bool ans ;
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        const Expanded(
-          flex: 5,
+        Expanded(
+          flex: 12,
           child: Center(
             child: Text(
-              "Questions will  be displayed here",
-              style: TextStyle(
+              questions[questionNumber],
+              style: const TextStyle(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
                 fontSize: 18,
@@ -30,24 +37,75 @@ class _QuizApp1State extends State<QuizApp1> {
           ),
         ),
         Expanded(
+          flex: 2,
           child: ElevatedButton(
             style: ElevatedButton.styleFrom(
               primary: Colors.green,
             ),
-            onPressed: () {},
-            child: const Text("button 1"),
+            onPressed: () {
+              setState(() {
+                ans=answers[questionNumber];
+                if(ans == true){
+                  scoreKeeper.add(
+                    const Icon(
+                      Icons.check_circle,
+                      color: Colors.green,
+                    ),
+                  );
+                }else{
+                  scoreKeeper.add(
+                    const Icon(
+                      Icons.cancel,
+                      color: Colors.red,
+                    ),
+                  );
+                }
+                if (questionNumber <= 2) {
+                  questionNumber++;
+                }
+              });
+            },
+            child: const Text("True"),
           ),
         ),
         const SizedBox(
           height: 10,
         ),
         Expanded(
+          flex: 2,
           child: ElevatedButton(
             style: ElevatedButton.styleFrom(
               primary: Colors.red,
             ),
-            onPressed: () {},
-            child: const Text("button 1"),
+            onPressed: () {
+              setState(() {
+                ans=answers[questionNumber];
+                if(ans == false){
+                  scoreKeeper.add(
+                    const Icon(
+                      Icons.check_circle,
+                      color: Colors.green,
+                    ),
+                  );
+                }else{
+                  scoreKeeper.add(
+                    const Icon(
+                      Icons.cancel,
+                      color: Colors.red,
+                    ),
+                  );
+                }
+                if (questionNumber <= 2) {
+                  questionNumber++;
+                }
+              });
+            },
+            child: const Text("False"),
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: scoreKeeper,
           ),
         ),
       ],
